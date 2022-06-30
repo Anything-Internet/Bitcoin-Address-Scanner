9afc58a0c45c7cc7fde1eb95a6b2d6b17d6b9485
JerryHobby <>
2022-06-30 16:28:15 +0000

    fixed dupes in txt

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import datetime
 import time
 import asyncio
-import tkinter as tk
+import mainapp
 from colorama import Fore, Back, Style, init
 import btcscan
 
-max_iterations = 1000
-concurrent_scanners = 5
+max_iterations = 10000
+concurrent_scanners = 10
 scanner = []
-
-for cnt in range(concurrent_scanners):
-    scanner.append(btcscan.btcscan(cnt))
 
 # place any addresses to manually include
 manual_addresses = [
@@ -26,24 +23,55 @@ manual_addresses = [
     },
 ]
 
-# load manual addresses in first instance only
-for manual in manual_addresses:
+
+##############################################
+# main loop
+
+async def main():
+    start = datetime.datetime.now()
+    print(f"{Fore.WHITE}started at {time.strftime('%X')}")
+
+    for cnt in range(concurrent_scanners):
+        scanner.append(btcscan.btcscan(cnt))
+
+    scan_manual()
+
+    for i in range(max_iterations):
+        tasks = []
+        for cnt in range(len(scanner)):
+            tasks.append(asyncio.create_task(scan_next(cnt)))
+            
+        await asyncio.wait(tasks)            
     
-    success = scanner[0].scan_address(
-        manual["pub_key"], manual["priv_key"], manual["address"])
+    stop = datetime.datetime.now()
+    print(f"\n{Fore.WHITE}finished: {(stop-start)/concurrent_scanners} secs per instance")
 
-    if success < 0:
-        print(f"[0]ERROR checking: {scanner[0].address}")
-    elif success == 0:
-        print(f"[0]EMPTY account: {scanner[0].address}")
-    else:
-        balance = '{0:.6f}'.format(float(scanner[0].final_balance/scanner[0].SATOSHIS_PER_BTC))
+
+##############################################
+# scan manual addresses to update values
+
+def scan_manual():
+    # load manual addresses in first instance only
+    for manual in manual_addresses:
         
-        short_address = scanner[0].address[0:5] + "..."
-        print(f"{Fore.WHITE}[0][{scanner[0].get_scan_counter()}] WINNER account: {short_address} Bal: {balance}", end="\n")
+        success = scanner[0].scan_address(
+            manual["pub_key"], manual["priv_key"], manual["address"])
+    
+        if success < 0:
+            print(f"[0]ERROR checking: {scanner[0].address}")
+        elif success == 0:
+            print(f"[0]EMPTY account: {scanner[0].address}")
+        else:
+            balance = '{0:.6f}'.format(float(scanner[0].final_balance/scanner[0].SATOSHIS_PER_BTC))
+            
+            short_address = scanner[0].address[0:5] + "..."
+            print(f"{Fore.WHITE}[0][{scanner[0].get_scan_counter()}] WINNER account: {short_address} Bal: {balance}", end="\n")
 
         
-# this needs to be asynchronous
+##############################################
+# scan_next invokes ONE scan per instance
+# invoked in a loop from main
+            
 async def scan_next(cnt):
     instance = scanner[cnt]
     success = instance.scan_random()
@@ -51,33 +79,25 @@ async def scan_next(cnt):
 
     if success < 0:
         print(f"{Fore.RED}[{cnt}][{instance.get_scan_counter()}] ERROR checking: {short_address}", end="\n")
+        
     elif success == 0:
         print(f"{Fore.BLUE}[{cnt}][{instance.get_scan_counter()}] EMPTY account: {short_address}", end="\r")
+        
     else:
         balance = '{0:.6f}'.format(float(instance.final_balance/instance.SATOSHIS_PER_BTC))
         print(f"{Fore.WHITE}[{cnt}][{instance.get_scan_counter()}] WINNER account: {short_address} Bal: {balance}", end="\n")
 
-# main loop / needs to support asynchronous
 
+##############################################
+# start application
 
-async def main():
-    start = datetime.datetime.now()
-    print(f"{Fore.WHITE}started at {time.strftime('%X')}")
+if __name__ == "__main__":
 
-    # run short sample and time it
-    for i in range(max_iterations):
-    #while True:
-        tasks = []
-        for cnt in range(len(scanner)):
-            tasks.append(asyncio.create_task(scan_next(cnt)))
-            
-        for cnt in range(len(scanner)):
-            await tasks[cnt]
-    
-    stop = datetime.datetime.now()
-    print(f"\n{Fore.WHITE}finished: {(stop-start)/concurrent_scanners} secs per instance")
+    graphics = False
 
-
-
-asyncio.run(main())
+    if graphics:
+        app = mainapp.mainApp()
+        app.mainloop()
+    else:
+        asyncio.run(main())
 
